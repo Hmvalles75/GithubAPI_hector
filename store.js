3998e9e1e74e59c56bda855f568565048e827cc4
Hmvalles75 <51212330+Hmvalles75@users.noreply.github.com>
2019-08-08 05:56:40 -0700

    added necessary object to store

---
+++ store.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const store = (function() {
+
+
+  /**
+   * 
+   */
+  function findRepoById(id) {
+    
+  }
+    
+
+  /**
+   * add repo to list of repositories for user.
+   * 
+   * @param {*}  repo an element of the json response to be added to store of repos
+   */
+  function addRepo(repo) {
+    this.repositories.push(
+      {
+        name: repo.name,
+        url : repo.url,
+      });
+  }
+    
+
+  // /**
+  //  * 
+  //  */
+  // function deleteRepo() {
+
+  // }
+
+
+
+  return {
+
+    repositories: [],
+
+    findRepoById,
+    addRepo,
+    // deleteRepo,
+
+  };
+
+})();
